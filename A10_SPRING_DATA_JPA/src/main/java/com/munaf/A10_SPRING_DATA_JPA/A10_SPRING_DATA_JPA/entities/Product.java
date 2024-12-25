28cac893cb35660131fd1120b4463cf97285d27f
MunafBadarpura <munafbadarpura11@gmail.com>
2024-12-25 15:38:49 +0530

    25/12/2024

--- A10_SPRING_DATA_JPA/src/main/java/com/munaf/A10_SPRING_DATA_JPA/A10_SPRING_DATA_JPA/entities/Product.java
+++ A10_SPRING_DATA_JPA/src/main/java/com/munaf/A10_SPRING_DATA_JPA/A10_SPRING_DATA_JPA/entities/Product.java
@@ -1,4 +1,54 @@
 package com.munaf.A10_SPRING_DATA_JPA.A10_SPRING_DATA_JPA.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique", columnNames = {"sku"}),
+                @UniqueConstraint(name = "name_price_unique", columnNames = {"name", "price"})
+                // name with price should be unique like if name parle with price 20 exist so new parle with price 50 is valid
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    @Column(name = "product_name", nullable = false, length = 25)
+    private String name;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
 }
---
+++ A10_SPRING_DATA_JPA/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO test.product_table (price, quantity, sku, product_name)
+VALUES
+(50.50, 5, 'parle123', 'Parle'),
+(60.66, 6, 'pepsi123', 'Pepsi'),
+(10.00, 10, 'chips123', 'Chips');
