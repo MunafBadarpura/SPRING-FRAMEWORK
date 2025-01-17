d1c40cf5e297964c3f8def3233df01540b7a7072
MunafBadarpura <munafbadarpura11@gmail.com>
2025-01-17 11:43:20 +0530

    17/01/2025

---
+++ A13_SPRING_SECURITY_1/src/main/java/com/munaf/A13_SPRING_SECURITY_1/A13SpringSecurity1Application.java
@@ -0,0 +1,13 @@
+package com.munaf.A13_SPRING_SECURITY_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A13SpringSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A13SpringSecurity1Application.class, args);
+	}
+
+}
---
+++ A13_SPRING_SECURITY_1/src/test/java/com/munaf/A13_SPRING_SECURITY_1/A13SpringSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munaf.A13_SPRING_SECURITY_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A13SpringSecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
